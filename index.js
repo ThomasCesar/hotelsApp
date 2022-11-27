43606714bc0b876c4a6b3b796845b5639b4bc4c6
Thomas FRANCOIS <thomas.francois.mail@gmail.com>
2022-11-27 14:45:47 +0100

    mise en place des vues, du Front end & infinite scroll

--- index.js
+++ index.js
@@ -1,9 +1,50 @@
-const app = require('express')()
+
+
+// ============ INIT MODULES
+
+// fetch app config
 const config = require('./config')
 
-app.get('/',(req,res)=>{
-    res.write('Hello there')
+// init express app
+const express = require('express')
+const app = express()
+app.use(express.static('public'))
+
+// ============ APP ROUTES
+
+const navMenu = [
+    {
+        link : 'home',
+        label : 'Accueil'
+    },
+    {
+        link : 'about',
+        label : 'A propos'
+    }
+]
+
+app.get(['/','/home'], (req, res) => {
+    res.render(
+        'home.ejs',
+        { navMenu }
+    )
+})
+
+// ============ SCRIPT ROUTES
+
+app.post('/hotels',(req,res)=>{
+    res.write('getting hotels')
     res.end()
 })
+
+// ============ 404 PAGE
+
+app.use(( req,res, next )=>{
+    res.status(404)
+    res.render('404.ejs',{ navMenu })
+    next()
+})
+
+// ============ LAUNCH SERVER
 
 app.listen( config.port )
